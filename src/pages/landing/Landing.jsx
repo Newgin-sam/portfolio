c41c43439fc7a24f647592e8108516a1742ff708
Newgin Sam <newginsam@gmail.com>
2021-04-12 02:31:58 +0530

    modified the link style for mobile view

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -6,6 +6,7 @@ const Landing = () => {
     const location = window.location.pathname;
 
     const [home,sethome] = useState('');
+    const [open,setopen] = useState(false);
 
     useEffect(() => {
         // console.log(location)
@@ -23,27 +24,38 @@ const Landing = () => {
         }
     }
     
+    const clickHam = () => {
+        setopen(!open);
+       
+    }
+
+    const hamclose = () => {
+        setopen({open:false});
+    }
+    
     return(
             <React.Fragment>
+                <div className={`${open ? styles.list__button_open : ''} ${styles.list__button}`} onClick={clickHam}>
+                    <div className={styles.list__button_ham}></div>
+                </div>
                 <div className={styles.header}>
                 <div className={ home === true ? `${styles.container}` : `${styles.container} ${styles.container__active}`}>
-                
-                    <h1 className={  home === true ? `${styles.heading1} ${styles.headingHome}` : `${styles.heading1} ${styles.heading1__active}` }><span>NEWGIN SAM E</span>NEWGIN SAM E<span>NEWGIN SAM E</span></h1>
-                    <h2 className={home === true ? `${styles.heading2}` : `${styles.heading2} ${styles.heading2__active}` }> A <span>Software Developer</span> with An Enthusiastic Attitude</h2>
-                    <ul className={ home === true ? `${styles.listContainer}` : `${styles.listContainer} ${styles.listContainer__active}` }>
-                        <li className={home === true ? `${styles.list} ${styles.list__1}` : `${styles.list__active} ${styles.list} ${styles.list__1}` }>
+                    <h1 className={ `${styles.heading1}  ${home === true ? styles.headingHome : styles.heading1__active}` }><span>NEWGIN SAM E</span>NEWGIN SAM E<span>NEWGIN SAM E</span></h1>
+                    <h2 className={ `${styles.heading2} ${home === true ? "" : styles.heading2__active}` }> A <span>Software Developer</span> with An Enthusiastic Attitude</h2>
+                    <ul className={ `${home === true ? "" : styles.listContainer__active} ${open === true ? styles.listContainer__open : '' } ${styles.listContainer}`}>
+                        <li className={ `${styles.list} ${styles.list__1} ${home === true ? "" : styles.list__active} ` } onClick={hamclose}>
                             <NavLink exact activeClassName = {styles.LinkActive} className={styles.link} to="/" onClick={() => handleClick(true)}>HOME</NavLink>
                         </li>
-                        <li className={home === true ? `${styles.list} ${styles.list__2}` : `${styles.list__active} ${styles.list} ${styles.list__2}` }>
+                        <li className={` ${styles.list} ${styles.list__2}  ${home === true ? "" : styles.list__active} ` }>
                             <NavLink activeClassName = {styles.LinkActive} className={styles.link} to="/about" onClick={() => handleClick(false)}>ABOUT</NavLink>
                         </li>
-                        <li className={home === true ? `${styles.list} ${styles.list__3}` : `${styles.list__active} ${styles.list} ${styles.list__3}` }>
+                        <li className={` ${styles.list} ${styles.list__3} ${home === true ? "" : styles.list__active} ` }>
                             <NavLink activeClassName = {styles.LinkActive} className={styles.link} to="/resume" onClick={() => handleClick(false)}>RESUME</NavLink>
                         </li>
-                        <li className={home === true ? `${styles.list} ${styles.list__4}` : `${styles.list__active} ${styles.list} ${styles.list__4}` }>
+                        <li className={` ${styles.list} ${styles.list__4}  ${home === true ? "" : styles.list__active} ` }>
                             <NavLink activeClassName = {styles.LinkActive} className={styles.link} to="/certification" onClick={() => handleClick(false)}>CERTIFICATIONS</NavLink>
                         </li>
-                        <li className={home === true ? `${styles.list} ${styles.list__5}` : `${styles.list__active} ${styles.list} ${styles.list__5}` }>
+                        <li className={` ${styles.list} ${styles.list__5} ${home === true ? "" : styles.list__active} ` }>
                             <NavLink activeClassName = {styles.LinkActive} className={styles.link} to="/contact" onClick={() => handleClick(false)}>CONTACT</NavLink>
                         </li>
                         
