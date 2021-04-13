fc7feb77ac119d26c4ebc8ae2c81a2a321ac1f16
Newgin Sam <newginsam@gmail.com>
2021-04-14 01:05:38 +0530

    icon added to footer

--- src/componenets/footer/Footer.jsx
+++ src/componenets/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import {FaHandHoldingHeart} from 'react-icons/fa'
 const Footer = (props) => {
     return (
         <div className={styles.container}>
-            <p className={styles.text}>Designed and Developed by <span className={styles.span}>Newgin Sam</span></p>
+            <p className={styles.text}>Designed and Developed with <FaHandHoldingHeart className={styles.icon}/>by <span className={styles.span}>Newgin Sam</span></p>
         </div>
     );
 }
