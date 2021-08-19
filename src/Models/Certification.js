b9c51ee239acd03391a3e6d17062c7558967e78f
Newgin Sam <newginsam@gmail.com>
2021-08-19 09:46:59 +0530

    skills & certification updated

--- src/Models/Certification.js
+++ src/Models/Certification.js
@@ -1,45 +1,51 @@
 const certification = [
     {
-        title:'Software Developer Intern, Solartis',
-        field:'intern',
-        description:"Completed 3 months Internship",
-        organization:'SOLARTIS'
+        title: 'Software Developer Intern, Solartis',
+        field: 'intern',
+        description: "Completed 3 months Internship",
+        organization: 'SOLARTIS'
     },
     {
-        title:'Software Developer Intern, Zoho',
-        field:'intern',
-        description:"Completed 1 months Internship",
-        organization:'ZOHO'
+        title: 'Software Developer Intern, Zoho',
+        field: 'intern',
+        description: "Completed 1 months Internship",
+        organization: 'ZOHO'
     },
     {
-        title:'Advanced CSS and Sass: Flexbox, Grid, Animations and More!',
-        field:'course',
-        description:"Completed 28 hours online course by Jonas Schmedtmann",
-        organization:'Udemy'
+        title: 'React practice course, learn by practicing ( 2021 edition )',
+        field: 'course',
+        description: "Completed 82 hours ( 50 hours new version + 32 hours old version ) online course by Coding Revolution",
+        organization: 'Udemy'
     },
     {
-        title:'Complete React Developer in 2021 (w/ Redux, Hooks, GraphQL)',
-        field:'course',
-        description:"Completed 39.5 hours online course by Andrei Neagoie, Yihua Zhang",
-        organization:'Udemy'
+        title: 'Advanced CSS and Sass: Flexbox, Grid, Animations and More!',
+        field: 'course',
+        description: "Completed 28 hours online course by Jonas Schmedtmann",
+        organization: 'Udemy'
     },
     {
-        title:'Core & Advanced Java ',
-        field:'course',
-        description:"Completed 40 hours offline course",
-        organization:'Silicon Software'
+        title: 'Complete React Developer in 2021 (w/ Redux, Hooks, GraphQL)',
+        field: 'course',
+        description: "Completed 39.5 hours online course by Andrei Neagoie, Yihua Zhang",
+        organization: 'Udemy'
     },
     {
-        title:'C Programming',
-        field:'Course',
-        description:"Completed 20 hours offline course",
-        organization:'NIIT'
-    }, 
+        title: 'Core & Advanced Java ',
+        field: 'course',
+        description: "Completed 40 hours offline course",
+        organization: 'Silicon Software'
+    },
     {
-        title:'C++ Programming',
-        field:'course',
-        description:"Completed 20 hours offline course",
-        organization:'NIIT'
+        title: 'C Programming',
+        field: 'Course',
+        description: "Completed 20 hours offline course",
+        organization: 'NIIT'
+    },
+    {
+        title: 'C++ Programming',
+        field: 'course',
+        description: "Completed 20 hours offline course",
+        organization: 'NIIT'
     }
 ]
 
--- src/Models/skills.js
+++ src/Models/skills.js
@@ -1,48 +1,54 @@
-import { FaReact,FaNodeJs,FaNode,FaJava } from "react-icons/fa";
-import { SiJavascript,SiMongodb,SiSass } from "react-icons/si";
-import {AiOutlineConsoleSql} from "react-icons/ai";
+import { FaReact, FaNodeJs, FaNode, FaJava } from "react-icons/fa";
+import { SiJavascript, SiMongodb, SiRedux, SiFirebase } from "react-icons/si";
+import { AiOutlineConsoleSql } from "react-icons/ai";
 
 const Skills = [
     {
-        head:"Programming languages :",
-        value:"Java, JavaScript, C, C++"
+        head: "Programming languages :",
+        value: "Java, JavaScript, C, C++"
     },
     {
-        head:"Web Development :",
-        value:"Reactjs, Nodejs, Expressjs, Redux, Hooks, scss"
+        head: "Web Development :",
+        value: "Reactjs, Nodejs, Expressjs, Redux, Hooks, scss"
     },
     {
-        head:"Version control system :",
-        value:"Github, Gitlab, SVN"
+        head: "Version control system :",
+        value: "Github, Gitlab, SVN"
     },
     {
-        head:"Database :",
-        value:"MySQL, PostgreSQL, MongoDB."
+        head: "Database :",
+        value: "MySQL, PostgreSQL, MongoDB."
     },
 ]
 
-export const aboutSkill = [ {
+export const aboutSkill = [{
     name: "REACT",
     Icon: FaReact
 }, {
+    name: "REDUX",
+    Icon: SiRedux
+}, {
     name: "NODE",
     Icon: FaNode
-},{
+}, {
     name: "EXPRESS",
     Icon: FaNodeJs
-},{
+}, {
     name: "SCSS",
     Icon: FaReact
-},{
+}, {
     name: "JAVASCRIPT",
     Icon: SiJavascript
-},{
+}, {
     name: "JAVA",
     Icon: FaJava
-},{
+}, {
+    name: "FIREBASE",
+    Icon: SiFirebase
+}, {
     name: "MONGODB",
     Icon: SiMongodb
-},{
+}, {
     name: "SQL",
     Icon: AiOutlineConsoleSql
 }]
