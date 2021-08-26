258a4ce6b9dbf6f2f4999ab76ccf1f90faaff0b3
Newgin Sam <newginsam@gmail.com>
2021-08-26 09:27:20 +0530

    about info added

---
+++ src/Models/aboutInfo.js
@@ -0,0 +1,23 @@
+export const Info = [
+    {
+        title: "BIRTH PLACE",
+        value: "Kanyakumari, Tamil Nadu, India"
+    },
+    {
+        title: "RESIDENCE",
+        value: "Chennai, Tamil Nadu, India"
+    },
+    {
+        title: "BORN",
+        html: true,
+        value: "28<sup>th</sup> feb' 1999"
+    },
+    {
+        title: "EDUCATION",
+        value: "Bachelor of Engineering"
+    },
+    {
+        title: "EMAIL",
+        value: "newginsam@gmail.com"
+    }
+]
