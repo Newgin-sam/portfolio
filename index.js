3637a9ad8a59e0b560ef17a2bcacfcfb717e3318
Newgin Sam <newginsam@gmail.com>
2021-09-20 15:24:13 +0530

    changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
