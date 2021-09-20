13cb09d2b73c105e49648302ac07ae4b6ce96450
Newgin Sam <newginsam@gmail.com>
2021-09-20 11:45:47 +0530

    static file route

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/api/mail', (req, res) => {
 
 app.use(express.static('client/build'));
 if (process.env.NODE_ENV === 'production') {
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'))
     });
 }
